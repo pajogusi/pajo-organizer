1a402fbfec4548b2b1ff95bb18cb973be47612ca
pajogusi <pajogusi.uk@gmail.com>
2026-06-25 23:23:11 +0100

    Criar dashboard inicial do Pàjó Organizer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("Pàjó Organizer iniciado.");
+console.log("Pàjó Organizer v0.2 carregado.");
+
+const textarea = document.querySelector("textarea");
+
+if(textarea){
+    textarea.value = localStorage.getItem("notaRapida") || "";
+
+    textarea.addEventListener("input",()=>{
+        localStorage.setItem("notaRapida", textarea.value);
+    });
+}
